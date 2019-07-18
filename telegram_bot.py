88c60d49707986790f321274e30538c393820180
gregdan3 <gregory.danielson3@gmail.com>
2019-07-18 17:48:54 -0500

    Enable logging; import necessary tools for weather, emotes

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
+import logging
+
+import dateparser
 from telegram.ext import Updater, CommandHandler
-from credentials import TOKEN
 from noaa_sdk import noaa
+from requests import get
+
+from credentials import TOKEN
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 
 def hello(bot, update):
